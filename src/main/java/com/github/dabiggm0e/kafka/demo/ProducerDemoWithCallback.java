9d00e0df1d7374cc8275b82091a02bece41740a6
moe <Mo@homebase>
2020-02-04 14:57:59 +0300

    Added a logger to the producer.send() callback

--- src/main/java/com/github/dabiggm0e/kafka/demo/ProducerDemoWithCallback.java
+++ src/main/java/com/github/dabiggm0e/kafka/demo/ProducerDemoWithCallback.java
@@ -2,11 +2,14 @@ package com.github.dabiggm0e.kafka.demo;
 
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerDemoWithCallback {
     public static void main(String[] args) {
+        Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
         String bootstrap_servers = "localhost:9092";
 
         // create producer properties
@@ -25,11 +28,16 @@ public class ProducerDemoWithCallback {
         producer.send(record, (recordMetadata, e) -> {
             // if record was sent successfully
             if (e==null) {
-
+                log.info("New record was produced with the following metadata:" + "\n"
+                        + "Topic: " + recordMetadata.topic() + "\n"
+                        + "Partition: " + recordMetadata.partition() + "\n"
+                        + "Offset: " + recordMetadata.offset() + "\n"
+                        + "Timestamp: " + recordMetadata.timestamp() + "\n"
+                );
             }
             //if there was an exception
             else {
-
+                log.error("There was an exception while producing.", e);
             }
         });
 
